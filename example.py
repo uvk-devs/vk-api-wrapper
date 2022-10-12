ffe9c126abc0eef111838d9c12d854aab5f8394c
aidgamesa <ya.ivanov1302@yandex.ru>
2022-10-12 01:54:21 +0000

    last fix

--- example.py
+++ example.py
@@ -2,4 +2,4 @@ from vklib import VK
 
 client=VK("client_id", "client_secret")
 client.auth("EMAIL OR PHONE NUMBER", "PASSWORD")
-client.api.wall.post(client.id, "Hello, world!")
+client.api.wall.post(owner_id=client.id, message="Hello, world!")
